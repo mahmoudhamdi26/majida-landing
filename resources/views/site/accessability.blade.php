a0b74d04cbe4e9a96189a83cc8ef1ce12803aba0
mahmo <m.hamdi@zedny.com>
2019-03-22 16:16:40 +0200

    Adding accessability tool

---
+++ resources/views/site/accessability.blade.php
@@ -0,0 +1,36 @@
+Copy / paste the accessibility statement below to your website as a new page under Majidah.org/accessibility. Feel free to customize it as you see fit to ensure it accurately reflects your website's accessibility efforts. Remember to link to it from your site's nav menu or footer.
+
+<h1>Majidah.org Accessibility Statement</h1>
+
+
+<br /><br />
+
+Updated: March 2019.
+
+<br /><br />
+
+<h2>General</h2>
+Majidah.org strives to ensure that its services are accessible to people with disabilities. Majidah.org has invested a significant amount of resources to help ensure that its website is made easier to use and more accessible for people with disabilities, with the strong belief that every person has the right to live with dignity, equality, comfort and independence.
+
+<h2>Accessibility on Majidah.org</h2>
+Majidah.org makes available the <a href="https://UserWay.org" alt="Free Website Accessibility Widget" title="Free Website Accessibility Widget">UserWay Website Accessibility Widget</a> that is powered by a dedicated accessibility server. The software allows Majidah.org to improve its compliance with the Web Content Accessibility Guidelines (WCAG 2.1).
+
+<h2>Enabling the Accessibility Menu</h2>
+The Majidah.org accessibility menu can be enabled by clicking the accessibility menu icon that appears on the corner of the page. After triggering the accessibility menu, please wait a moment for the accessibility menu to load in its entirety.
+
+<h2>Disclaimer</h2>
+Majidah.org continues its efforts to constantly improve the accessibility of its site and services in the belief that it is our collective moral obligation to allow seamless, accessible and unhindered use also for those of us with disabilities.
+
+<br /><br />
+
+Despite our efforts to make all pages and content on Majidah.org fully accessible, some content may not have yet been fully adapted to the strictest accessibility standards. This may be a result of not having found or identified the most appropriate technological solution.
+
+<h2>Here For You</h2>
+If you are experiencing difficulty with any content on Majidah.org or require assistance with any part of our site, please contact us during normal business hours as detailed below and we will be happy to assist.
+
+<h2>Contact Us</h2>
+If you wish to report an accessibility issue, have any questions or need assistance, please contact Majidah.org Customer Support as follows:
+
+<br /><br />
+
+Email: <a href="mailto:mahmoud.h26@hotmail.com">mahmoud.h26@hotmail.com</a>
